07660d1846c2367b0147bfafa1cdbac6841c7c5f
cecisalof <cecilia.sanchezlosada@gmail.com>
2025-01-15 09:51:48 +0100

    [Feature]: Record Model & Migration files

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Record extends Model
 {
-    //
+    //use HasFactory;
+
+    protected $fillable = [
+        'title', 'artist', 'release_year', 'status', 'price', 'cover_image'
+    ];
+
+    public function genres()
+    {
+        /* define relación muchos a muchos entre dos modelos. 
+        * En este caso, un Record (disco de vinilo) puede pertenecer a varios Genre (géneros musicales) y viceversa.
+        */
+        return $this->belongsToMany(Genre::class);
+    }
 }
--- database/migrations/2025_01_15_083412_create_records_table.php
+++ database/migrations/2025_01_15_083412_create_records_table.php
@@ -13,12 +13,19 @@ return new class extends Migration
     {
         Schema::create('records', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 255);
+            $table->string('artist', 255);
+            $table->year('release_year');
+            $table->enum('status', ['nuevo', 'usado', 'colección']);
+            $table->decimal('price', 8, 2);
+            $table->string('cover_image', 255);
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     * el método down() elimina todas las tablas creadas en el método up() para revertir los cambios si es necesario.
      */
     public function down(): void
     {
