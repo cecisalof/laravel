18162921c6e9a7f41527f1b1e2e542ee8b73a9b1
cecisalof <cecilia.sanchezlosada@gmail.com>
2025-01-16 11:10:33 +0100

    [Feature]: Home page Controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Record;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        // Obtener los primeros 5 discos de vinilo
+        $records = Record::take(5)->get();  // Esto debe adaptarse según la lógica real para obtener discos.
+
+        return view('home', compact('records'));
+    }
+}
