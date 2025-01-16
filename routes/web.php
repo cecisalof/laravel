0566181bcd75e9ae39358dbdbf7650a0dcf59d12
cecisalof <cecilia.sanchezlosada@gmail.com>
2025-01-16 11:10:19 +0100

    [Feature]: Home page route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');  // Página de inicio
 
 Route::get('/dashboard', function () {
     return view('dashboard');
