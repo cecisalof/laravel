c1d582a616a850178d460eb4d9af543a625a249e
cecisalof <cecilia.sanchezlosada@gmail.com>
2025-01-17 10:24:06 +0100

    [Fix]: refactor  x-app-layout/x-slot to @extends/@section

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,15 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!-- Extiende el layout principal app.blade.php -->
+@extends('layouts.app')
 
+<!-- Define la sección del encabezado que contiene el título del Dashboard -->
+@section('header')
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        {{ __('Dashboard') }}
+    </h2>
+@endsection
+
+<!-- Define el contenido principal de la página -->
+@section('content')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -14,4 +19,5 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
+
