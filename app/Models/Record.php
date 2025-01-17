e2b48a2eba1a5217c52384cfe939f6d37eea390a
cecisalof <cecilia.sanchezlosada@gmail.com>
2025-01-17 11:42:22 +0100

    [Fix]: enabling the factory funcionality

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Record extends Model
 {
-    //use HasFactory;
+    use HasFactory;
 
     protected $fillable = [
         'title', 'artist', 'release_year', 'status', 'price', 'cover_image'
