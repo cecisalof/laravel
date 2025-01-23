7ef72a0e737517d49f0b1eca28f3d2d40366b7b3
cecisalof <cecilia.sanchezlosada@gmail.com>
2025-01-23 09:09:47 +0100

    [Fix]: imageUrl.includes('via.placeholder.com') conditional statement

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,34 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Seleccionar todas las imágenes de la página
+    let images = document.querySelectorAll('img');
+    console.log(images);
+    
+
+    images.forEach(function (img) {
+        // Cuando la imagen se carga correctamente
+        img.onload = function () {
+            checkPlaceholderImage(img);
+        };
+
+        // Si ocurre un error al cargar la imagen
+        img.onerror = function () {
+            this.src = '/storage/records/set-vinyl-records.jpg'; // Ruta de la imagen por defecto
+        };
+    });
+});
+
+
+function checkPlaceholderImage(img) {
+    let imageUrl = img.src;
+    console.log(imageUrl)
+
+    // Verificar si la URL de la imagen es un marcador de posición
+    if (imageUrl.includes('via.placeholder.com')) {
+        img.src = 'public/storage/records/set-vinyl-records.jpg';
+    }
+}
