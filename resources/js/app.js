3791c4876eeed456ec2fa8579b2eda56243c800e
cecisalof <cecilia.sanchezlosada@gmail.com>
2025-01-23 10:24:42 +0100

    [Fix]: code optimization

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,27 +9,30 @@ Alpine.start();
 
 document.addEventListener('DOMContentLoaded', function () {
     // Seleccionar todas las imágenes de la página
-    let images = document.querySelectorAll('img');
-    console.log(images);
-    
+    let images = document.querySelectorAll('img');    
 
     images.forEach(function (img) {
+        // Si ocurre un error al cargar la imagen
+        img.onerror = function () {
+            this.src = '/storage/records/set-vinyl-records.jpg'; // Ruta de la imagen por defecto
+        };
+        
         // Cuando la imagen se carga correctamente
         img.onload = function () {
             checkPlaceholderImage(img);
         };
 
-        // Si ocurre un error al cargar la imagen
-        img.onerror = function () {
-            this.src = '/storage/records/set-vinyl-records.jpg'; // Ruta de la imagen por defecto
-        };
+        // Comprobar si la imagen es un marcador de posición (placeholder) cuando ya se haya cargado
+        if (img.complete && img.src.includes('via.placeholder.com')) {
+            checkPlaceholderImage(img);
+        }
     });
 });
 
 
 function checkPlaceholderImage(img) {
     let imageUrl = img.src;
-    console.log(imageUrl)
+    console.log('URL de la imagen:', imageUrl);
 
     // Verificar si la URL de la imagen es un marcador de posición
     if (imageUrl.includes('via.placeholder.com')) {
